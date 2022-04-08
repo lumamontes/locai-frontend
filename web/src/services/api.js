c045391299b30fef5113e45ca2555953d826b70f
Luma Montes <lumagoesmontes@gmail.com>
2022-04-07 22:46:33 -0300

    adicionado estrutura comentada para tratamento de respostas da api

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -4,3 +4,17 @@ export const api = axios.create({
   baseURL:"http://localhost:8080/api",
   Authorization: ''
 })
+//TODO: descomentar lógica e criar  estrutura para interceptacao de requests(ex,sessão ou token expirada, tratamento de errors em request para api etc)
+// api.interceptors.response.use(
+//   function (response) {
+//     return response;
+//   }, 
+//   function (error) {
+//     let res = error.response;
+//     if (res.status == 401) {
+//       window.location.href = "http://localhost:3000/"
+//     }
+//     console.error(res.status);
+//     return Promise.reject(error);
+//   }
+// );
