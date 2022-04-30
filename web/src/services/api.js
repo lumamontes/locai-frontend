226a44a65bd1d4835451bc4e4631da048636fbbc
Luma Montes <lumagoesmontes@gmail.com>
2022-04-30 17:00:42 -0300

    iniciado tratamento para erros do servidor com toasts

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,20 +1,30 @@
 import axios from "axios";
+import { toast } from 'react-toastify';
 
 export const api = axios.create({
   baseURL: process.env.REACT_APP_BASEURL,
   Authorization: ''
 })
 //TODO: descomentar lógica e criar  estrutura para interceptacao de requests(ex,sessão ou token expirada, tratamento de errors em request para api etc)
-// api.interceptors.response.use(
-//   function (response) {
-//     return response;
-//   }, 
-//   function (error) {
-//     let res = error.response;
-//     if (res.status == 401) {
-//       window.location.href = "http://localhost:3000/"
-//     }
-//     console.error(res.status);
-//     return Promise.reject(error);
-//   }
-// );
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    console.log(error.response, 'uuu')
+    switch(error.response.status){
+      //TODO: adicionar tratamento 
+      // case 401:
+      // break 
+      // case 500:
+      // break
+      default:
+        toast.error(`${error.response.data.message}`);
+      break;
+    }
+ 
+    if (error.response.status == 400) {
+    }
+    return Promise.reject(error);
+  }
+);
