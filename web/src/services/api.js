de3604e3c1b3056d5bbd6967104e091893164de2
Luma Montes <lumagoesmontes@gmail.com>
2022-05-09 20:05:59 -0300

    comentado interceptador

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -3,6 +3,7 @@ import { toast } from 'react-toastify';
 
 export const api = axios.create({
   baseURL:'https://tcc-backend-w3aet.ondigitalocean.app/api/',
+  // baseURL:'http://localhost:8080/api/',
   Authorization: ''
 })
 //TODO: descomentar lógica e criar  estrutura para interceptacao de requests(ex,sessão ou token expirada, tratamento de errors em request para api etc)
@@ -11,19 +12,19 @@ api.interceptors.response.use(
     return response;
   },
   (error) => {
-    console.log(error.response.data, 'uuu')
-    switch(error.response.status){
-      //TODO: adicionar tratamento quit
-      // case 401:
-      // break 
-      // case 500:
-      // break
-      default:
-        toast.error(`${error.response.data.message}`, {
-          hideProgressBar:true
-        });
-      break;
-    }
+    // console.log(error.response.data, 'uuu')
+    // switch(error.response.status){
+    //   //TODO: adicionar tratamento quit
+    //   // case 401:
+    //   // break 
+    //   // case 500:
+    //   // break
+    //   default:
+    //     toast.error(`${error.response.data.message}`, {
+    //       hideProgressBar:true
+    //     });
+    //   break;
+    // }
  
     if (error.response.status == 400) {
     }
